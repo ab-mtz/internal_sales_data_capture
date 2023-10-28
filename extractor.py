7f27eead155bc26ae80dc0d43e55dbf865f6c062
ab-mtz <bmtzs@me.com>
2023-10-28 23:39:24 +0200

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -1,5 +1,22 @@
+from PIL import Image
+from pytesseract import pytesseract
+
+# Define path to tesseract.exec
+path_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+path_image = 'images/sample-sample-text-1.jpg'
+
+# Point tesseract_cmd to tesseract.exe
+pytesseract.tesseract_cmd = path_tesseract
+
+
 def main():
     # Read image
+    img = Image.open(path_image)
+    # Extract text 
+    text = pytesseract.image_to_string(img)
+
+    print(text)
     # run tesseract
     # extract text
     # search for regular expresions
