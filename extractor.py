aa2e530e4047422f0cabf8e8fba057e66b7b51b4
ab-mtz <bmtzs@me.com>
2023-10-28 23:14:09 +0200

    Update extractor.py

---
+++ extractor.py
@@ -0,0 +1,14 @@
+def main():
+    # Read image
+    # run tesseract
+    # extract text
+    # search for regular expresions
+        # Bestellungs nummer
+        # datum
+        # €
+    # pack info 
+    # conect to google sheet api 
+    # instert into to fields
+
+if __name__ == "__main__":
+    main()
