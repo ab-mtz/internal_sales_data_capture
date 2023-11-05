0257e9201a852bfd9dfd017fad4666c90fc7213e
ab-mtz <bmtzs@me.com>
2023-11-05 21:12:44 +0100

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -16,7 +16,7 @@ def main():
     # search for regular expresions
     for line in results:
         ic(line)
-        _, content_, _2 = line
+        _, content, _2 = line
         ic(content)
         # Bestellungs nummer
         # pattern = 'de\d*'
