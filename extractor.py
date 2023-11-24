3db98b4cab4ef7b2a35c49c98710b0e8396d538e
ab-mtz <bmtzs@me.com>
2023-11-24 11:14:53 +0100

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -23,11 +23,10 @@ def main():
 
     # Load image
     image_path = 'images\sample2.jpg'
+
     # extract text
     results = reader.readtext(image_path)
-    ic(results)
     
-    # for i in range(3)
     # search for regular expresions
     bestellung = ""
     datum = ""
@@ -39,15 +38,19 @@ def main():
         bestellung_pattern = r'de\d*'
         date_pattern = r'^\d{2}\.\d{2}\.\d{4}$'
         zahlung_pattern = r'\d*.?\d+ *€'
+
         if match := re.search(bestellung_pattern, content):
             bestellung = match[0]
             ic(match)
+
         if match := re.search(date_pattern, content):
             datum = validate_date(match[0])
             ic(match)
+
         if match := re.search(zahlung_pattern, content):
             zahlung = match[0]
             ic(match)
+
         if bestellung and datum and zahlung:
             data.append([bestellung, datum, zahlung, current_date])
             bestellung = ""
@@ -55,23 +58,35 @@ def main():
             zahlung = ""
 
     filename = "output.csv"
-    # If file already exists
+
+    
     try:
-    # Try to open the CSV file in append mode ('a')
-        with open(filename, mode='a', newline='') as file:
+    # If file already exists
+        with open(filename, mode='a+', newline='') as file:
             writer = csv.writer(file)
             
-            # Writing the data to the CSV file
-            writer.writerows(data)
-        print(f"The CSV file '{filename}' has been updated.")
+             file.seek(0, 2)  # Move the cursor to the end of the file
+            file_empty = file.tell() == 0
 
-    except FileNotFoundError:
-        # If the file doesn't exist, create a new CSV file
-        with open(filename, 'w', newline='') as file:
+            # Create a CSV writer object
             writer = csv.writer(file)
-            writer.writerow(header)
-            writer.writerows(data)
-        print(f"The CSV file '{filename}' has been created.")
+
+            # Write the header if the file is empty
+            if file_empty:
+                writer.writerow(header)
+                # Writing the data to the CSV file
+                writer.writerows(data)
+                print(f"The CSV file '{filename}' has been created.")
+            else:
+                writer.writerows(data)
+                print(f"The CSV file '{filename}' has been updated.")
+
+    # except FileNotFoundError:
+    #     # If the file doesn't exist, create a new CSV file
+    #     with open(filename, 'w', newline='') as file:
+    #         writer = csv.writer(file)
+    #         writer.writerow(header)
+    #         writer.writerows(data)
 
     # pack info 
     # conect to google sheet api 
